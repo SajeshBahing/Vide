f3bf7eadc3974b81fddd7d7d05b23da13bdbb58d
Sajesh bahing <sajesh.bahing22@gmail.com>
2017-04-19 23:52:33 +0545

    Decided to exclude limelight plugin for now

---
+++ installer.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+pushd `dirname $0` > /dev/null
+directory=`pwd -P`
+popd > /dev/null
+
+today=`date +%Y%m%d`
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    if [ ! -d ~/.vim/bundle/ ]; then
+        mkdir ~/.vim/bundle
+    fi
+
+    cd ~/.vim/bundle
+
+    echo "downloading plugins from git repo."
+
+    git clone $line
+    
+done < "repo"
+
+echo "Backing up system's .vimrc file and creating new one."
+
+if [ ! -d ~/.vim/bak/ ]; then
+    mkdir ~/.vim/bak/
+fi
+
+cd $directory
+
+if [ ! -f .vimrc ]; then
+    echo "Please update git repo completely, no configuration file is found"i
+    exit 1
+fi
+
+cp ~/.vimrc ~/.vim/bak/vimrc-$today
+cp .vimrc ~/.vimrc 
+
+if [ ! -d extend_vimrc ]; then
+    echo "Please update git repo completely, not all configuration file is found"
+    exit 1
+fi
+
+cp -R extend_vimrc ~/.vim/
